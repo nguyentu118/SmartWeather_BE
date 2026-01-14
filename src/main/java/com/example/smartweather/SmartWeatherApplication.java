6caa34e75cd98fdf96f29fc99adbd13c1b1440ac
nguyentu118 <tu147537@gmail.com>
2026-01-14 13:42:03 +0700

    ttrien khai du an

---
+++ src/main/java/com/example/smartweather/SmartWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWeatherApplication.class, args);
+	}
+
+}
